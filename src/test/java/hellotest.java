4fcdbf51f7c2f121832fa5ba40582f2d1da50f5c
ZZT <420812731@qq.com>
2022-06-13 14:43:23 +0800

    hot-fix second commit

--- src/test/java/hellotest.java
+++ src/test/java/hellotest.java
@@ -3,5 +3,7 @@ public class hellotest {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello3");
+        System.out.println("hello3");
     }
 }
